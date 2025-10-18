f1a9a50e7dfebc4991b67cd983b0b6f838b16589
Milk <tylerf66@gmail.com>
2025-10-17 20:20:12 -0500

    made articles own page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,8 +13,7 @@
 		document.title = 'Ty Fischer';
 		unsubscribe = selectedArticle.subscribe(async () => {
 			await tick();
-			document.documentElement.scrollTop = 0;
-			document.body.scrollTop = 0;
+			window.scrollTo({ top: 0, left: 0, behavior: 'instant' });
 		});
 	});
 	onDestroy(() => {
